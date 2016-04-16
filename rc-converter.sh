7d9c14a51f1c2bb94accf80980194ac50dde730d
Adam Szakal <adam.b.szakal@gmail.com>
2016-04-16 21:17:22 +0200

    improved behaviour

--- rc-converter.sh
+++ rc-converter.sh
@@ -4,7 +4,10 @@ if [ -f ~/.vimrc ]
 then
     cp ~/.vimrc ~/.vimrc-backup
     echo "Created .vimrc-backup"
+    echo -e "source ~/Repos/dotfiles/_vimrc" > ~/.vimrc
+    echo "Added source to _vimrc in .vimrc"
+else 
+    echo -e "source ~/Repos/dotfiles/_vimrc" > ~/.vimrc
+    echo "Created ~/.vimrc which links to ./_vimrc"
 fi
 
-echo -e "source ~/Repos/dotfiles/_vimrc" > ~/.vimrc
-echo "Added source to _vimrc in .vimrc"
